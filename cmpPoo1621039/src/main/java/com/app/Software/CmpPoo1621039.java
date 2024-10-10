a44aa87dba99d6c618fb39beac72dc4925b6bfc7
adisonzenemij <adisonzenemij@gmail.com>
2024-10-10 18:35:54 -0500

    feat: trabajo colaborativo

---
+++ cmpPoo1621039/src/main/java/com/app/Software/CmpPoo1621039.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.app.Software;
+
+/**
+ *
+ * @author adiso
+ */
+public class CmpPoo1621039 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
